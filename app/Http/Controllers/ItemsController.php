650456279f11fc06750b738948b24dddb68f9d68
yurure32 <yurure32@gmail.com>
2018-10-08 12:09:54 +0000

    want

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -38,4 +38,15 @@ class ItemsController extends Controller
             'items' => $items,
         ]);
     }
+    
+    public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -26,4 +26,49 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    
+    public function items()
+    {
+        return $this->belongsToMany(Item::class)->withPivot('type')->withTimestamps();
+    }
+    
+    public function want_items()
+    {
+        return $this->items()->where('type', 'want');
+    }
+    
+    public function want($itemId)
+    {
+        $exist = $this->is_wanting($itemId);
+        
+        if($exist) {
+            return false;
+        } else {
+            $this->items()->attach($itemId, ['type' => 'want']);
+            return true;
+        }
+    }
+    
+    public function dont_want($itemId)
+    {
+        $exist = $this->is_wanting($itemId);
+        
+        if ($exist) {
+            \DB::delete("DELETE FROM item_user WHERE user_id = ? AND item_id = ? AND type = 'want'", [$this->id, $itemId]);
+        } else {
+            return false;
+        }
+        
+    }
+    
+    public function is_wanting($itemIdOrCode)
+    {
+        if (is_numeric($itemIdOrCode)) {
+            $item_id_exists = $this->want_items()->where('item_id', $itemIdOrCode)->exists();
+            return $item_id_exists;
+        } else {
+            $item_code_exists = $this->want_items()->where('code', $itemIdOrCode)->exists();
+            return $item_code_exists;
+        }
+    }
 }
