1e91a154819e23d96d68fc2408b8ea2b9da4d6a1
yurure32 <yurure32@gmail.com>
2018-10-15 03:51:33 +0000

    ranking

--- resources/views/items/items.blade.php
+++ resources/views/items/items.blade.php
@@ -22,8 +22,8 @@
                         </div>
                         @if (isset($item->count))
                             <div class="panel-footer">
-                                <p class="text-center">{{ $key+1 }}位: {{ $item->count }} Wants</p>
-                                <p class="text-center">{{ $key+1 }}位: {{ $item->count }} Haves</p>
+                                <p class="text-center">{{ $key+1 }}位: {{ $item->count }} {{ $type }}</p>
+                                <!--<p class="text-center">{{ $key+1 }}位: {{ $item->count }} Haves</p>-->
                             </div>
                         @endif
                     </div>
